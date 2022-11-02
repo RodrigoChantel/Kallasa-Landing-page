817ba50557c20f03ae18ada96af272d9f2c01cde
RodrigoChantel <rodrigochantel@gmail.com>
2022-11-02 18:24:31 -0300

    Main update

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="http://localhost/kallasa.com.br/assets/css/style.css" rel="stylesheet" type="text/css">
+    <link href="http://localhost/kallasa.com.br/assets/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="http://localhost/kallasa.com.br/assets/img/favicon/favicon.ico">
+    <title>Document</title>
+</head>
+<body>
+
+
+    
+</body>
+<script src="http://localhost/kallasa.com.br/assets/js/popper.min.js"></script>
+<script src="http://localhost/kallasa.com.br/assets/js/style.js"></script>
+<script src="http://localhost/kallasa.com.br/assets/js/bootstrap.min.js"></script>
+<script src="http://localhost/kallasa.com.br/assets/js/fontawesome.js" crossorigin="anonymous"></script>
+<script>const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))</script>
+</html>
