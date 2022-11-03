df7f989ecfdaabc5f53c3938ba828c16b007373b
RodrigoChantel <rodrigochantel@gmail.com>
2022-11-03 00:48:35 -0300

    New update

--- index.php
+++ index.php
@@ -4,20 +4,137 @@
 <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="http://localhost/kallasa.com.br/assets/css/style.css" rel="stylesheet" type="text/css">
-    <link href="http://localhost/kallasa.com.br/assets/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="icon" type="image/x-icon" href="http://localhost/kallasa.com.br/assets/img/favicon/favicon.ico">
-    <title>Document</title>
+    <link href="http://192.168.0.100/kallasa.com.br/assets/css/style.css" rel="stylesheet" type="text/css">
+    <link href="http://192.168.0.100/kallasa.com.br/assets/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="http://192.168.0.100/kallasa.com.br/Assets/img/favicon/favicon.ico">
+    <title>Kallasa Mkt</title>
 </head>
 <body>
+    <header>
+        <nav class="navbar navbar-expand-lg bg-light py-3 border-bottom">
+            <div class="container-fluid container-sm">
+            <a class="navbar-brand text-dark fw-bold" href="#"><i class="fa-sharp fa-solid fa-rocket"></i> Kallasa.Mkt</a>
 
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="fa-sharp fa-solid fa-bars text-dark"></i>
+                </button>
 
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mx-auto mb-2 mb-lg-0">
+                        <li class="nav-item mx-2">
+                            <a class="nav-link active" aria-current="page" href="#">Planos</a>
+                        </li>
+
+                        <li class="nav-item dropdown mx-2">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Produtos
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Identidade Visual</a></li>
+                                <li><a class="dropdown-item" href="#">Sites e Sistemas</a></li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li><a class="dropdown-item" href="#">Treinamentos</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="nav-item dropdown mx-2">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Soluções
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="nav-item mx-2">
+                            <a class="nav-link" href="#">Consultoria</a>
+                        </li>
+
+                        <li class="nav-item mx-2">
+                            <a class="nav-link">Sobre nós</a>
+                        </li>
+                    </ul>
+                    <div class="center-ctn">
+                        <button class="btn-ctn">
+                            <svg width="150px" height="40px" viewBox="0 0 150 40" class="border-ctn">
+                            <polyline points="179,1 179,59 1,59 1,1 179,1" class="bg-line" />
+                            <polyline points="179,1 179,59 1,59 1,1 179,1" class="hl-line" />
+                            </svg>
+                            <span><i class="fa-sharp fa-solid fa-headset"></i>&nbsp;&nbsp;CONTATO</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
     
+    <main>
+        <section class="section-01 w-100 bg-light d-flex flex-row flex-nowrap justify-content-center" style="height: 600px;">
+            <div class="container-lg h-100 d-flex flex-row flex-wrap justify-content-between align-items-center">
+                
+                <div class="container-fluid mx-md-0 mx-auto px-0 asd" style="width: 400px;">
+                    <h5 class="card-title fs-1 text-white">Você não precisa dar conta de tudo!</h5><br>
+                    <p class="card-text fs-5 text-light">Nós estamos aqui para facilitar a sua vida e fazer a sua empresa voar.</p><br>
+                    <div class="container-fluid d-flex flex-row flex-wrap justify-content-between px-0">
+                        <a class="btn btn-primary py-2" href="#">Vamos conversar</a>
+                        <a class="btn btn-light px-5 py-2" href="#">Planos</a>
+                    </div>
+                </div>
+
+                <div class="container-fluid mx-md-0 mx-auto p-0" style="width: 300px;">
+                    <div class="kallasa-box d-flex">
+                        <div class="d-flex kallasa-edit-01">
+                        </div>
+                        <div class="kallasa-box-left bg-primary kallasa-edit-02"></div>
+                        <div class="kallasa-box-right bg-primary kallasa-edit-02"></div>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+
+        <section class="section-02" style="height: 600px;">
+            <p class="text-center mt-5 fs-3 fw-bold">Nossas soluções se baseiam em</p>
+            <div class="container-lg py-3 mt-5 d-flex flex-row flex-wrap justify-content-around">
+
+                <div class="container-fluid mx-md-0 mx-auto p-0" style="width: 300px;">
+                    <div class="kallasa-box-solutions bg-dark d-flex">
+                        <div class="bg-secondary kallasa-box-solutions-01"></div>
+                        <div class="bg-secondary kallasa-box-solutions-02"></div>
+                    </div>
+                </div>
+
+                <div class="card text-center pt-5 pb-3 my-lg-0 my-md-0 my-3 shadow-sm" style="width: 18rem;">
+                    <div class="card-body">
+                        <img class="img-fluid" src="https://kallasa.000webhostapp.com/Assets/img/icons/Analytics.png" style="width: 100px;" alt="">
+                        <h5 class="card-title mt-3">Analytics</h5>
+                    </div>
+                </div>
+
+                <div class="card text-center pt-5 pb-3 my-lg-0 my-md-0 my-3 shadow-sm" style="width: 18rem;">
+                    <div class="card-body">
+                        <img class="img-fluid" src="https://kallasa.000webhostapp.com/Assets/img/icons/Prevcc.png" style="width: 100px;" alt="">
+                        <h5 class="card-title mt-3">Previsão do comportamento do cliente</h5>
+                    </div>
+                </div>
+
+                <div class="card text-center pt-5 pb-3 my-lg-0 my-md-0 my-3 shadow-sm" style="width: 18rem;">
+                    <div class="card-body">
+                        <img class="img-fluid" src="https://kallasa.000webhostapp.com/Assets/img/icons/EO.png" style="width: 100px;" alt="">
+                        <h5 class="card-title mt-3">Estratégia orientada pelo ROI</h5>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
 </body>
-<script src="http://localhost/kallasa.com.br/assets/js/popper.min.js"></script>
-<script src="http://localhost/kallasa.com.br/assets/js/style.js"></script>
-<script src="http://localhost/kallasa.com.br/assets/js/bootstrap.min.js"></script>
-<script src="http://localhost/kallasa.com.br/assets/js/fontawesome.js" crossorigin="anonymous"></script>
+<script src="http://192.168.0.100/kallasa.com.br/assets/js/popper.min.js"></script>
+<script src="http://192.168.0.100/kallasa.com.br/assets/js/style.js"></script>
+<script src="http://192.168.0.100/kallasa.com.br/assets/js/bootstrap.min.js"></script>
+<script src="./Assets/js/fontawesome.js" crossorigin="anonymous"></script>
 <script>const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))</script>
 </html>
